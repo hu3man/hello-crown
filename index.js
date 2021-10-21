478385ff87f5f25877a9b75070508b673036659f
Houston K <32914364+hu3man@users.noreply.github.com>
2021-10-20 17:51:00 -0700

    Implemented thought detection for tongue movement

---
+++ index.js
@@ -0,0 +1,54 @@
+const { Notion } = require("@neurosity/notion");
+require("dotenv").config();
+const deviceId = process.env.DEVICE_ID || "";
+const email = process.env.EMAIL || "";
+const password = process.env.PASSWORD || "";
+
+const verifyEnvs = (email, password, deviceId) => {
+    const invalidEnv = (env) => {
+        return env === "" || env === 0;
+    };
+    if (
+        invalidEnv(email) ||
+        invalidEnv(password) ||
+        invalidEnv(deviceId)
+    ) {
+        console.error(
+            "Please verify deviceId, email, and password are in .env file. Quitting program..."
+        );
+        process.exit(0);
+    }
+};
+verifyEnvs(email, password, deviceId);
+console.log(`${email} attempting to authenticate to ${deviceId}`);
+
+const notion = new Notion({
+    deviceId
+});
+
+
+const main = async() => {
+    await notion
+        .login({
+            email,
+            password
+        })
+        .catch(error => {
+            console.log(error);
+            throw new Error(error);
+        });
+    console.log("Logged In")
+
+    notion.kinesis("tongue").subscribe((intent) => {
+        console.log("Tongue thought detected: " + JSON.stringify(intent));
+    });
+
+    // notion.calm().subscribe((calm) => {
+    //     if(calm.probability > 0.3) {
+    //         console.log("Calm level hit 0.3!!!");
+    //     }
+    // })
+};
+
+main();
+
